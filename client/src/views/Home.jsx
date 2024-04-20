867658b1ca8f8461323a38e8af4386cb903ccf9b
Morgan Ruiz-Demaree <120768738+mruizdem@users.noreply.github.com>
2024-04-20 18:27:03 -0500

    added client

---
+++ client/src/views/Home.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const Home = (props) => {
+	return (
+		<div>
+			<h1 className="text-3xl font-bold underline text-center mt-10">
+				Home View Testing!
+			</h1>
+		</div>
+	);
+};
+
+export default Home;
